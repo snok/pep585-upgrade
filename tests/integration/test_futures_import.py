2a33df0768df63d1df4a0c106fbbf4fe411ecca7
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2021-01-23 10:03:56 +0100

    Create failing test

---
+++ tests/integration/test_futures_import.py
@@ -0,0 +1,24 @@
+from tests.integration import int_test
+
+example = """
+from __future__ import annotations
+
+from typing import List
+
+x: List
+"""
+
+expected = """
+from __future__ import annotations
+
+x: list
+"""
+
+
+def test_no_duplicate_futures_annotations():
+    """
+    Originally forgot to check for existing futures imports before adding a new one.
+
+    Creating this to avoid this in the future.
+    """
+    int_test(example, expected, futures=True)
